b2cbf3fe4b8fa50a9def6592d8fa1f0685f8513f
markus <mlachmann@labor-lademannbogen.de>
2016-03-11 13:04:15 +0100

    logger

--- src/main/java/deltaanalytics/bruker/controller/GetVersionController.java
+++ src/main/java/deltaanalytics/bruker/controller/GetVersionController.java
@@ -1,12 +1,15 @@
 package deltaanalytics.bruker.controller;
 
 import deltaanalytics.bruker.hardware.CommandRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetVersionController {
+    private Logger LOGGER = LoggerFactory.getLogger(GetVersionController.class);
     @Autowired
     private CommandRunner commandRunner;
 
@@ -16,7 +19,7 @@ public class GetVersionController {
         try {
             result = commandRunner.getVersion();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("",e);
             result = "Error " + e.getMessage();
         }
         return result;
