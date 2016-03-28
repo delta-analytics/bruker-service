d042e3b1b29119be3aeb0c6211b44a8d9e0be137
markus <mlachmann@labor-lademannbogen.de>
2016-03-28 13:38:45 +0200

    Controller Rest Methods

--- src/main/java/deltaanalytics/bruker/controller/MeasureReferenceController.java
+++ src/main/java/deltaanalytics/bruker/controller/MeasureReferenceController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class MeasureReferenceController {
     private Logger LOGGER = LoggerFactory.getLogger(MeasureReferenceController.class);
     private CommandRunner commandRunner;
 
-    @RequestMapping("/measureReference")
+    @RequestMapping(value = "/measureReference", method = RequestMethod.POST)
     public void measureReference() {
         LOGGER.info("measureReference");
         commandRunner.measureReference();
